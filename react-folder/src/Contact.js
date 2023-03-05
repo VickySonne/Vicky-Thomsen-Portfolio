958164a489337a03b913e65273d3a5a1e32420fe
VickySonne <vst1996@live.dk>
2023-03-05 22:37:36 +0100

    download button styled and react-icons installed

--- react-folder/src/Contact.js
+++ react-folder/src/Contact.js
@@ -1,8 +1,5 @@
 import SectionHeading from "./SectionHeading";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
-import {faPhone, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {FaEnvelope} from "react-icons/fa";
 
 const Contact = () => {
     return (
@@ -11,19 +8,20 @@ const Contact = () => {
                 <SectionHeading title={"kontakt."} text={"Kontakt mig via sociale medier, mobil eller email"} />
                 <div>
                     <a href="https://www.linkedin.com/" target="_blank">
-                        <FontAwesomeIcon icon={faLinkedin} color="red" />
+                        
                         <p>LinkedIn</p>
                     </a>
                 </div>
                 <div>
                     <a href="tel:+4542923630">
-                        <FontAwesomeIcon icon={faPhone} color="red" />
+                        
                         <p>42 92 36 30</p>
                     </a>
                 </div>
                 <div>
+                    <FaEnvelope />
                     <a href="mailto:vst1996@live.dk">
-                        <FontAwesomeIcon icon={faEnvelope} color="red" />
+                        
                         <p>vst1996@live.dk</p>
                     </a>
                 </div>
