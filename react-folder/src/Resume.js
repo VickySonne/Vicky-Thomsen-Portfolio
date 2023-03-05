5633d6be55a1352463f377deeab478d1553950f0
VickySonne <vst1996@live.dk>
2023-03-05 21:56:48 +0100

    download cv function added

--- react-folder/src/Resume.js
+++ react-folder/src/Resume.js
@@ -1,11 +1,19 @@
 import SectionHeading from "./SectionHeading";
+import FileSaver from 'file-saver';
 
 const Resume = () => {
+    const downloadCv = () => {
+        FileSaver.saveAs(
+            process.env.PUBLIC_URL + "/resource/testcv.pdf",
+            "VickySonneThomsenCV.pdf"
+        )
+    }
+    
     return (
         <section className="section-dark">
             <div className="resume whitespace">
                 <SectionHeading title={"resume."} text={"Download mit CV"} />
-                <button>Download</button>
+                <button className="download-cv" onClick={downloadCv}>Download CV</button>
             </div>
         </section>
     );
