bb0d926f1d9fd06da14261cd5919e91856a41819
VickySonne <vst1996@live.dk>
2023-03-18 13:35:59 +0100

    ul and li added to navbar

--- react-folder/src/NavBar.js
+++ react-folder/src/NavBar.js
@@ -8,21 +8,58 @@ const NavBar = () => {
         });
     }
 
+    // const sectionAbout = document.querySelector('#about');
+    // // // const sectionSkills = document.querySelector('#skills');
+    // // // const sectionContact = document.querySelector('#contact');
+
+    // // // const sectionArray = [sectionAbout, sectionSkills, sectionContact];
+
+    // const options = {};
+
+    // const observer = new IntersectionObserver(function(entries, observer){
+    //         entries.forEach(entry => {
+    //             console.log(entry);
+    //         })
+    // }, options);
+
+    // // observer.observe(sectionAbout);
+
+
     return (
         <header>
             <nav className="nav-bar whitespace">
                 <div className="name hide media-481-show">
-                    <a onClick={() => {handleClick("home")}}><h1>Vicky Sonne Thomsen</h1></a>
+                    <ul>
+                        <li>
+                        <a onClick={() => {handleClick("home")}}><h1>Vicky Sonne Thomsen</h1></a>
+                        </li>
+                    </ul>
                 </div>
-                <div className="links">
-                    <a onClick={() => {handleClick("about")}}>om mig</a>
-                    <a onClick={() => {handleClick("skills")}}>kompetencer</a>
-                    <a onClick={() => {handleClick("contact")}}>kontakt</a>
+                <div >
+                    <ul className="links">
+                        <li>
+                            <a className="menu-about" onClick={() => {handleClick("about")}}>om mig</a>
+                        </li>
+                        <li>
+                            <a className="menu-skills" onClick={() => {handleClick("skills")}}>kompetencer</a>
+                        </li>
+                        <li>
+                            <a className="menu-contact" onClick={() => {handleClick("contact")}}>kontakt</a>
+                        </li>
+                    </ul>
                 </div>
-                <div className="icons hide">
-                    <a href="https://www.linkedin.com/in/vicky-thomsen-8533ba239/" target="_blank"><FaLinkedinIn size="1.5em" color="#FFF"/></a>
-                    <a href="tel:+4542923630"><FaPhone size="1.5em" color="#FFF"/></a>
-                    <a href="mailto:vst1996@live.dk"><FaEnvelope size="1.5em" color="#FFF"/></a>
+                <div className="hide">
+                    <ul className="icons">
+                        <li>
+                            <a href="https://www.linkedin.com/in/vicky-thomsen-8533ba239/" target="_blank" rel="noreferrer"><FaLinkedinIn size="1.5em" color="#FFF"/></a>
+                        </li>
+                        <li>
+                            <a href="tel:+4542923630"><FaPhone size="1.5em" color="#FFF"/></a>
+                        </li>
+                        <li>
+                            <a href="mailto:vst1996@live.dk"><FaEnvelope size="1.5em" color="#FFF"/></a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
